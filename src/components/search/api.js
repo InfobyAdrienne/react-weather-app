b9bf5bcfc8a6c844650adbe5fd8984a63ecf1af8
Adrienne Galloway <aasgalloway@gmail.com>
2023-10-24 17:01:02 +0100

    working on adding the API

---
+++ src/components/search/api.js
@@ -0,0 +1,3 @@
+const API_KEY = "dc4fdbc331baa8446855a7623e759098"
+
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?"
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { API_KEY, API_URL} from '../search/api'
 
 const Search = ({onSearchChange}) => {
   
   const[search, setSearch] = useState(null);
   
+  const loadOptions = (inputValue) => {
+    return fetch(`${API_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`, {
+      "method": "GET",
+      "headers": {
+      }
+      })
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  }
+
   const handleOnChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
@@ -16,6 +28,7 @@ const Search = ({onSearchChange}) => {
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   )
 }
