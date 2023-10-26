c6b7488718f1090d402aa56566264da71d502565
Adrienne Galloway <aasgalloway@gmail.com>
2023-10-26 23:09:27 +0100

    creating the CurrentWeather component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import Search from './components/search/search';
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current_weather";
 
 function App() {
-  
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
-  }
-  
+    console.log(searchData);
+  };
+
   return (
     <div className="container">
+      {/* Title */}
+      <h1 className="title">Weather App</h1>
+
       <Search onSearchChange={handleOnSearchChange} />
-      
+      < CurrentWeather />
 
-
-      
     </div>
-    
-    
   );
 }
 
---
+++ src/components/current_weather.js
@@ -0,0 +1,12 @@
+const CurrentWeater = () => {
+  return (
+    <div className="top">
+      <p className="city">London</p>
+      <p className="temperature">30°C</p>
+      <p className="description">Sunny</p>
+      <img alt="weather" className="weather-icon" src=""/>
+    </div>
+  );
+};
+
+export default CurrentWeater;
